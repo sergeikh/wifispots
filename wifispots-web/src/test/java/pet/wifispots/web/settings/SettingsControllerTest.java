4ec24ff946102ca214d43ab0ea2c5a0846d4c2c3
sergeikh <sergei.kholobuda@gmail.com>
2017-03-07 10:42:26 +0900

    Fixed settings controller unit test for settings edition

--- wifispots-web/src/test/java/pet/wifispots/web/settings/SettingsControllerTest.java
+++ wifispots-web/src/test/java/pet/wifispots/web/settings/SettingsControllerTest.java
@@ -1,31 +1,24 @@
 package pet.wifispots.web.settings;
 
+import org.hamcrest.CoreMatchers;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.ui.ExtendedModelMap;
+import org.springframework.ui.Model;
 import pet.wifispots.data.model.settings.Settings;
 import pet.wifispots.data.model.settings.SpotsAdditionMode;
 import pet.wifispots.data.repository.SettingsRepository;
 
-import static org.hamcrest.Matchers.hasProperty;
-import static org.hamcrest.Matchers.is;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static junit.framework.TestCase.assertEquals;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.Mockito.*;
 
 public class SettingsControllerTest {
     private SettingsController cut;
     private Settings settings;
-    private MockMvc mockMvc;
+    private Model model;
 
     @Mock
     SettingsRepository settingsRepository;
@@ -40,33 +33,26 @@ public class SettingsControllerTest {
         cut = new SettingsController();
         cut.settingsRepository = settingsRepository;
 
-        mockMvc =  MockMvcBuilders.standaloneSetup(cut).build();
+        model = new ExtendedModelMap();
     }
 
     @Test
     public void testViewSettings() throws Exception {
         when(settingsRepository.findOne(1l)).thenReturn(settings);
 
-        mockMvc.perform(get("/settings"))
-            .andExpect(status().isOk()).andDo(print())
-            .andExpect(view().name("settings/settings"))
-            .andExpect(model().attribute("settings", is(settings)))
-            .andExpect(model().attribute("settings", hasProperty("additionMode", is(SpotsAdditionMode.ONLY_REGISTERED_USERS))));
+        assertEquals(cut.editSettings(model), "settings/settings");
+        assertThat(model.asMap().keySet(), CoreMatchers.hasItems("settings", "usersRegistrationMode") );
 
         verify(settingsRepository, times(1)).findOne(1L);
     }
 
     @Test
     public void testUpdateSettings() throws  Exception {
-        settings.setId(1l);
         settings.setReverseGeocoderURL("test");
 
-        mockMvc.perform(post("/settings")
-                .param("reverseGeocoderURL", "test")
-                .param("additionMode", "ONLY_REGISTERED")
-                .param("viewMode", "EVERY_ONE")
-                .param("registrationMode", "EVERY_ONE"))
-            .andExpect(status().is3xxRedirection());
+        assertEquals(cut.updateSettings(settings), "redirect:/welcome");
+
+        assertEquals((long) settings.getId(), 1);
 
         verify(settingsRepository, times(1)).save(settings);
     }
